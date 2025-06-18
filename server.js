7bfd7853d79b5925c9f63ac5330fb8b5051e935d
mauryasoni2201 <maurya@krishaweb.com>
2025-06-18 18:19:25 +0530

    add code

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const corsOptions = {
     origin: 'http://localhost:3000',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-    // allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
 app.use(cors(corsOptions));
